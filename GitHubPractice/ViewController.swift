a3d4a4814ef1bc6b95da43a0fb4d10536123d7a3
Joel John <jjohn3593@stu.d214.org>
2022-04-04 13:28:34 -0500

    Add operation

--- GitHubPractice/ViewController.swift
+++ GitHubPractice/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    @IBOutlet weak var operationLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func whenAddButtonPressed(_ sender: UIButton) {
+        let firstNumber = firstNumberTextField.text!
+        let secondNumber = secondNumberTextField.text!
+        
+        let NumberFirst = Int(firstNumber)!
+        let  NumberSecond = Int(secondNumber)!
+        
+        let sum = NumberFirst + NumberSecond
+        
+        answerLabel.text = "\(sum)"
+        
+        operationLabel.text = "+"
+     
+        
+        
+    }
+    
 
+    
 
 }
 
